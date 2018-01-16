79e2de7e043cfc45b99d9a7f2b8e3251507df8c0
RN Kushwaha <rn.kushwaha022@gmail.com>
2018-01-16 09:12:11 +0530

    Added Symfony Routes
    Added Symfony Routes to the framework

--- src/Core/Handler.php
+++ src/Core/Handler.php
@@ -1,29 +1,77 @@
 <?php
 
 namespace Cruzer\Framework\Core;
+use Tracy\Debugger;
+use DebugBar\StandardDebugBar;
+
+/**
+ * This class handles request by dunamic passing routeInfo to the serve method
+ * @author RN Kushwaha <Rn.kushwaha022@gmail.com>
+ * @since version 1. Date: 9th Jan, 2018
+ */
 
 class Handler {
-
-	public function __construct( array $loadedModules=array(), array $route = array() ){
-
-	}
+	protected $debugbar;
 
 	public function serve( $routeInfo ) {
         list( $controller, $action )  = explode('@', $routeInfo[1]);
         $params                       = $routeInfo[2];
-        $controllerStr = "\\App\\Controllers\\".$controller;
+        $controllerStr = "\\".APP_DIR."\\".CONTROLLER_DIR."\\".$controller;
         $controllerObj = new $controllerStr();
-        $controllerObj->$action();
+        $controllerObj->$action($params);
 	}
 
-    public function hanldeError( $error_type, $msg )
-	{
-		if( ERROR_LEVEL == 'development' ){
-            trigger_error($msg, E_USER_ERROR);
-            // echo $msg;
-        }
-        header("HTTP/1.0 404 Not Found");
-        exit;
+	public function initilize( ) {
+		//include tracy
+		Debugger::enable(Debugger::DETECT, LOG_DIR);
+		Debugger::$strictMode = true;
+
+		// Debugger::$logSeverity = E_NOTICE | E_WARNING;
+		// Debugger::$email = 'admin@example.com';
+
+		/* Debugger::log('Unexpected error'); // text message
+		// Debugger::log($e, Debugger::ERROR); // also sends an email notification
+
+		// Debugger::dump([10, 20.2, true, null, 'hello']);
+
+		// //log errors to the chrom console.
+		// Debugger::fireLog('Hello World');
+		// Debugger::fireLog($_SERVER);
+		// Debugger::fireLog(new Exception('Test Exception'));
+		// Debugger::timer();
+		// sleep(2);
+		// echo $elapsed = Debugger::timer();*/
+
+		//use monolog for logging
+		// use Monolog\Logger;
+		// use Monolog\Handler\StreamHandler;
+
+		// // create a log channel
+		// $logger = new Logger('Cruzer');
+		// $logger->pushHandler(new StreamHandler(__DIR__ . '/log/cruzer.log', Logger::DEBUG));
+
+		// // add records to the log
+		// $logger->warning('Foo');
+		// $logger->error('Bar');
+		// $logger->info('My logger is now ready');
+		// $logger->info('Adding a new user', array('username' => 'Seldaek'));
+
+		//get dotenv
+		$dotenv = new \Dotenv\Dotenv(DIR);
+		$dotenv->load();
+	}
+
+	public function enableDebuger(){
+		return $this->debugbar = new StandardDebugBar();
+	}
+
+	public function renderDebugBar(){
+		$debugbarRenderer = $this->debugbar->getJavascriptRenderer()->setBaseUrl(DEBUG_DIR);
+		echo "<html> <head>". $debugbarRenderer->renderHead(). "</head> <body> ".$debugbarRenderer->render()."</body> </html>";
+	}
+
+	public function addMessage( $message, $tab = 'messages'){
+		$this->debugbar[$tab]->addMessage($message);
 	}
 
 }
---
+++ src/Core/Routes.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Cruzer\Framework\Core;
+
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\EventListener\RouterListener;
+use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+
+/**
+ * This class handles request by dunamic passing routeInfo to the serve method
+ * @author RN Kushwaha <Rn.kushwaha022@gmail.com>
+ * @since version 1. Date: 15th Jan, 2018
+ */
+
+class Routes implements HttpKernelInterface{
+
+	protected $routes;
+	protected $dispatcher;
+
+	public function __construct() {
+		$this->routes     = new RouteCollection();
+		$this->dispatcher = new EventDispatcher();
+	}
+
+	public function handle(Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true) {
+		$matcher            = new UrlMatcher($this->routes, new RequestContext());
+		$dispatcher         = new EventDispatcher();
+		$dispatcher->addSubscriber(new RouterListener($matcher, new RequestStack()));
+
+		$controllerResolver = new ControllerResolver();
+		$argumentResolver   = new ArgumentResolver();
+		$kernel             = new HttpKernel($dispatcher, $controllerResolver, new RequestStack(), $argumentResolver);
+		$response           = $kernel->handle($request );
+		$response->send();
+
+		$kernel->terminate($request, $response);
+	}
+
+	public function map($path, $controller) {
+		$this->routes->add( $path, new Route( $path, array('_controller' => $controller )));
+	}
+
+	public function on($event, $callback) {
+		 $this->dispatcher->addListener($event, $callback);
+	}
+
+	public function fire($event)
+    {
+	    return $this->dispatcher->dispatch($event);
+	}
+
+
+	public function getRequest()
+    {
+	    return $request = Request::createFromGlobals();
+	}
+
+}
+
